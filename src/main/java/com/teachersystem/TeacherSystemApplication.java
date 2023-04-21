a0105888d57907eed2daeed344a679f114bb4463
Vijaykumar <vijaykumarchougule2001@gmail.com>
2023-04-21 13:13:10 +0530

    first commit

---
+++ src/main/java/com/teachersystem/TeacherSystemApplication.java
@@ -0,0 +1,13 @@
+package com.teachersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherSystemApplication.class, args);
+	}
+
+}
